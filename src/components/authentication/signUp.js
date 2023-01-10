637000930538e677783dfde6fb98a63f588a0987
netzerothinkweb@gmail.com <netzerothinkweb@gmail.com>
2023-01-10 23:21:47 +0530

    m15

--- src/components/authentication/signUp.js
+++ src/components/authentication/signUp.js
@@ -1,32 +1,31 @@
-import { Fragment ,useState} from "react";
+import { Fragment, useState } from "react";
 import "./login.css";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser,faLock,faBuilding,faCheck } from "@fortawesome/free-solid-svg-icons";
 import { getAuth, createUserWithEmailAndPassword , updateProfile ,sendEmailVerification  } from "firebase/auth";
 import {db} from "../../firebase-config"
 import { doc, setDoc  } from "firebase/firestore"; 
 
-const signUp = () => 
-{
- 
- 
+const signUp = () => {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const [formData, setFormData] = useState({
     firstName: "",
     lastName: "",
     companyCode: "",
-    email:"",
+    email: "",
     password: "",
     confirmPassword: "",
   });
 
-  const {firstName,lastName,companyCode,email,password,confirmPassword} = formData;
+  const { firstName, lastName, companyCode, email, password, confirmPassword } =
+    formData;
 
-  const formChange = (e) =>{
-    setFormData((previousState)=>({
-      ...previousState,[e.target.id] :e.target.value
-    }))
+  const formChange = (e) => {
+    setFormData((previousState) => ({
+      ...previousState,
+      [e.target.id]: e.target.value,
+    }));
+  };
 
   }
  
@@ -47,6 +46,7 @@ const signUp = () =>
         displayName: user.displayName,
         photoURL: user.photoURL,
       })
+
     }
 
       
@@ -54,82 +54,127 @@ const signUp = () =>
       delete formDatacopy.password;
       delete formDatacopy.confirmPassword;
       console.log("Details updated ");
-      await setDoc(doc(db,'users',user.uid),formDatacopy);
-
-  } catch (error) {
-    console.log(error);
+      await setDoc(doc(db, "users", user.uid), formDatacopy);
+    } catch (error) {
+      console.log(error);
     }
 
- }
 
   return (
     <Fragment>
-      <div className="loginForm pr-[3rem]">
+      <div className="loginForm">
         <form onSubmit={onSubmit}>
-            <input
-              id = "firstName"
-              placeholder="First Name"
-              name="FirstName"
-              value={firstName}
-              className="pl-[4rem] py-5 block  mb-[1rem] border-2 border-black"
-              style={{ paddingRight: "20rem", fontSize: "1.1rem" }}
-              onChange = {formChange}
-              required
-            /> 
-            <input
-              id = "lastName"
-              placeholder="Last Name"
-              name="LastName"
-              value={lastName}
-              className="pl-[4rem] py-5 block  border-2 border-black mb-[1rem]"
-              style={{ paddingRight: "20rem", fontSize: "1.1rem" }} onChange = {formChange}
-              required
+          <input
+            id="firstName"
+            placeholder="First Name"
+            name="FirstName"
+            value={firstName}
+            className="pl-[4rem] py-5 inline  mb-[1rem] mr-[0.4rem] border-2 border-black authip w-[20.5rem] "
+            style={{ fontSize: "1.1rem" }}
+            onChange={formChange}
+            required
+          />
+          <input
+            id="lastName"
+            placeholder="Last Name"
+            name="LastName"
+            value={lastName}
+            className="pl-[4rem] py-5 inline  border-2 border-black mb-[1rem] authip w-[20.5rem]"
+            style={{ fontSize: "1.1rem" }}
+            onChange={formChange}
+            required
+          />
+          <div>
+            <FontAwesomeIcon
+              icon={faBuilding}
+              className="absolute ml-[2rem] mt-[1.7rem] text-xl"
             />
-            <FontAwesomeIcon icon={faBuilding} className="absolute ml-[2rem] mt-[1.7rem] text-xl"/>
             <input
-              id = "companyCode"
+              id="companyCode"
               value={companyCode}
               name="CompanyCode"
               placeholder="Company Code"
-              className="pl-[4rem] py-5 block  border-2 border-black mb-[1rem]"
-              style={{ paddingRight: "20rem", fontSize: "1.1rem" }} onChange = {formChange}
-              required
-            /> 
-              <FontAwesomeIcon icon={faUser} className="absolute ml-[2rem] mt-[1.7rem] text-lg"/>
-            <input
-              id = "email"
-              value={email}
-              name = "Email"
-              placeholder="Enter Your Email"
-              className="pl-[4rem] py-5 block border-2 border-black mb-[1rem]"
-              style={{ paddingRight: "20rem", fontSize: "1.1rem" }} onChange = {formChange}
+              className="pl-[4rem] py-5 inline mr-[0.4rem]  border-2 border-black mb-[1rem] authip w-[20.5rem]"
+              style={{ fontSize: "1.1rem" }}
+              onChange={formChange}
               required
             />
-              <FontAwesomeIcon icon={faLock} className="absolute ml-[2rem] mt-[1.7rem] text-lg"/>
+            {/* <input
+              id = "companyName"
+              placeholder="Company Name"
+              name="companyName"
+              value=""
+              className="pl-[4rem] py-5 inline  border-2 border-black mb-[1rem] authip w-[20.5rem]"
+              style={{ fontSize: "1.1rem" }} onChange = {formChange}
+              required
+            /> */}
+            <select
+              id="countries"
+              class="mb-[1rem] authip border-2 border-black text-gray-500 text-sm  focus:ring-blue-500 focus:border-blue-500 py-5 pl-[4rem] dark:bg-gray-700 dark:border-gray-600   dark:focus:ring-blue-500 dark:focus:border-blue-500 inline w-[20.5rem]"
+              style={{ fontSize: "1.1rem" }}
+            >
+              <option selected>Company Name</option>
+              <option value="US">United States</option>
+              <option value="CA">Canada</option>
+              <option value="FR">France</option>
+              <option value="DE">Germany</option>
+            </select>
+          </div>
+          <div>
+            <FontAwesomeIcon
+              icon={faUser}
+              className="absolute ml-[2rem] mt-[1.7rem] text-lg"
+            />
             <input
-              id = "password"
+              id="email"
+              value={email}
+              name="Email"
+              placeholder="Enter Your Email"
+              className="pl-[4rem] py-5 block border-2 border-black mb-[1rem] authip w-[41.4rem]"
+              style={{ fontSize: "1.1rem" }}
+              onChange={formChange}
+              required
+            />
+          </div>
+          <div>
+            <FontAwesomeIcon
+              icon={faLock}
+              className="absolute ml-[2rem] mt-[1.7rem] text-lg"
+            />
+            <input
+              id="password"
               name="Password"
               value={password}
               placeholder="Enter Your Password"
-              className="pl-[4rem] py-5 block border-2 border-black mb-[1rem]"
-              style={{ paddingRight: "20rem", fontSize: "1.1rem" }} onChange = {formChange}
+              className="pl-[4rem] py-5 block border-2 border-black mb-[1rem] authip w-[41.4rem]"
+              style={{ fontSize: "1.1rem" }}
+              onChange={formChange}
               required
             />
-            <FontAwesomeIcon icon={faCheck} className="absolute ml-[2rem] mt-[1.7rem] text-xl"/>
-              <input
-              id = "confirmPassword"
+          </div>
+          <div>
+            <FontAwesomeIcon
+              icon={faCheck}
+              className="absolute ml-[2rem] mt-[1.7rem] text-xl"
+            />
+            <input
+              id="confirmPassword"
               value={confirmPassword}
-
               placeholder="Confirm Password"
-              className="pl-[4rem] py-5 block border-2 border-black"
-              style={{ paddingRight: "20rem", fontSize: "1.1rem" }} onChange = {formChange}
+              className="pl-[4rem] py-5 block border-2 border-black authip w-[41.4rem]"
+              style={{ fontSize: "1.1rem" }}
+              onChange={formChange}
               required
             />
-            <div className="logFormBottom mt-8 flex justify-center">
-              <button className="loginButton font-bold text-xl text-white" >
-                Sign Up
-              </button>
-            </div>
+          </div>
+          <div
+            className="logFormBottom mt-8 flex"
+            style={{ justifyContent: "center" }}
+          >
+            <button className="loginButton font-bold text-xl text-white">
+              Sign Up
+            </button>
+          </div>
         </form>
         {/* <div className="altLogin flex mt-[4rem] justify-between w-[10rem]">
           <p className="text-lg" style={{ color: "darkgray" }}>
@@ -142,7 +187,6 @@ const signUp = () =>
         </div> */}
       </div>
     </Fragment>
-
   );
 };
 export default signUp;
